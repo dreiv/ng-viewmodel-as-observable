89011006319a92229162d65548be586fb1802d7a
Andrei Voicu <dreivoicu@gmail.com>
2020-04-12 14:22:04 +0300

    fix build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { PersonsComponent } from './persons/persons.component';
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent],
+  declarations: [AppComponent, CounterComponent, PersonsComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
